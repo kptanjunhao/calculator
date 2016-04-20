76b8b32b6581e349c1d2f0d7ae36e49bc4645fce
kptanjunhao <244296129@qq.com>
2016-04-20 15:38:25 +0800

    Optimize the method of PLUS, the old version used the double type to plus,may lead to some precision lose.The new version use a algorithm of LongNumberAdding.You can plus two long number and the precision will never gone.

--- calcular/ViewController.swift
+++ calcular/ViewController.swift
@@ -8,6 +8,110 @@
 
 import UIKit
 
+infix operator ++{
+    associativity left precedence 140
+}
+func ++(left:String,right:String) -> String{
+    var resultstr = String()
+    
+    var leftstr = [String]()
+    var rightstr = [String]()
+    if left.containsString("."){
+        leftstr = left.componentsSeparatedByString(".")
+    }else{
+        leftstr = [left]
+    }
+    if right.containsString("."){
+        rightstr = right.componentsSeparatedByString(".")
+    }else{
+        rightstr = [right]
+    }
+    //处理小数部分
+    //小数状态0代表左右都有小数部分，1代表左边有小数右边没，2代表右边有小数左边没，3代表两边都没有小数
+    let decimalStatu = leftstr.count == 2 && rightstr.count == 2 ? 0 : (leftstr.count == 1 && rightstr.count == 1 ? 3 : leftstr.count == 2 && rightstr.count != 2 ? 1 : 2)
+    var decimalCanPlus1 = false
+    switch decimalStatu {
+    case 0:
+        resultstr.appendContentsOf(".")
+        let leftIsLonger = leftstr[1] > rightstr[1]
+        var pstrlong = [Int]()
+        var pstrshort = [Int]()
+        for char in (leftIsLonger ? leftstr[1] : rightstr[1]).characters{
+            pstrlong.insert(Int(String(char))!, atIndex: 0)
+        }
+        for char in (!leftIsLonger ? leftstr[1] : rightstr[1]).characters{
+            pstrshort.insert(Int(String(char))!, atIndex: 0)
+        }
+        for index in pstrshort.count..<pstrlong.count{
+            resultstr.insert(Character(String(pstrlong[index])), atIndex: resultstr.startIndex.advancedBy(1))
+        }
+        for index in 0..<pstrshort.count{
+            var curResult = pstrlong[index] + pstrshort[index] + (decimalCanPlus1 ? 1 : 0)
+            if curResult >= 10{
+                curResult -= 10
+                decimalCanPlus1 = true
+            }else{
+                decimalCanPlus1 = false
+            }
+            resultstr.insert(Character(String(curResult)), atIndex: resultstr.startIndex.advancedBy(1))
+        }
+        
+    case 1:
+        resultstr.appendContentsOf(".")
+        for curNum in leftstr[1].characters{
+            resultstr.append(curNum)
+        }
+    case 2:
+        resultstr.appendContentsOf(".")
+        for curNum in rightstr[1].characters{
+            resultstr.append(curNum)
+        }
+    case 3:
+        break
+    default:
+        fatalError("decimalStatu can not detect two numbers statu")
+    }
+    //处理整数部分
+    var leftarray = [Int]()
+    for char in leftstr[0].characters{
+        leftarray.insert(Int(String(char))!, atIndex: 0)
+    }
+    var rightarray = [Int]()
+    for char in rightstr[0].characters{
+        rightarray.insert(Int(String(char))!, atIndex: 0)
+    }
+    let minCount = leftarray.count>rightarray.count ? rightarray.count : leftarray.count
+    var isGreaterThanTen = false
+    for index in 0..<minCount{
+        var curResult = leftarray[index] + rightarray[index] + (isGreaterThanTen ? 1 : 0) + (decimalCanPlus1 ? 1 : 0)
+        isGreaterThanTen = curResult >= 10
+        decimalCanPlus1 = false
+        if isGreaterThanTen{curResult -= 10}
+        resultstr.insert(Character(String(curResult)), atIndex: resultstr.startIndex)
+    }
+    let longArray = leftarray.count>rightarray.count ? leftarray : rightarray
+    for index in minCount..<(longArray.count){
+        var curResult = longArray[index]
+        if isGreaterThanTen{
+            curResult += 1
+            isGreaterThanTen = curResult >= 10
+            if isGreaterThanTen{
+                curResult -= 10
+            }
+        }
+        resultstr.insert(Character(String(curResult)), atIndex: resultstr.startIndex)
+    }
+    if isGreaterThanTen{
+        resultstr.insert("1", atIndex: resultstr.startIndex)
+    }
+    
+    
+    
+    
+    return resultstr
+}
+
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tableViewData: NSMutableArray!
@@ -19,6 +123,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var tempArray: NSMutableArray!
     var symbolArray: NSMutableArray!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +146,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.view.addSubview(numTF)
         symbolTF = UITextField(frame: CGRectMake(20,50,self.view.frame.width-40,30))
         symbolTF.textAlignment = .Right
+        
         self.view.addSubview(symbolTF)
         lastNumLabel = UILabel(frame: CGRectMake(20,30,self.view.frame.width-40,30))
         lastNumLabel.textAlignment = .Right
@@ -113,6 +220,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         numpView.backgroundColor = UIColor.whiteColor()
         numpView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.numTap(_:))))
         keyboardView.addSubview(numpView)
+        // 回删的视图添加
+        let curdX = 1 + 2 * btnWidth
+        let curdY = 1 + 3 * btnHeight
+        let numdViewFrame = CGRectMake(curdX, curdY, btnWidth - 1, btnHeight)
+        let numdView = UIView(frame: numdViewFrame)
+        let numdLabel = UILabel(frame: CGRectMake(0,0,numdViewFrame.width,numdViewFrame.height))
+        numdLabel.text = "回删"
+        numdLabel.textColor = UIColor.blueColor()
+        numdLabel.textAlignment = .Center
+        numdView.addSubview(numdLabel)
+        numdView.backgroundColor = UIColor.whiteColor()
+        numdView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.deleteNum)))
+        keyboardView.addSubview(numdView)
         
         for i in 0..<4{
             var symbol = ""
@@ -191,12 +311,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func addNumToTemp(sender:UIButton){
-        var num = Double()
+        var num = String()
         if numTF.text?.characters.last == "."{
             numTF.text = String(numTF.text?.characters.dropLast())
         }
-        if let tempnum = Double(numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")){
-            num = tempnum
+        if let _ = Double(numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")){
+            num = numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")
         }else{
             if tempArray.count != 0{
                 symbolArray.removeLastObject()
@@ -249,13 +369,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         symbolTF.placeholder = symbol
     }
     
-    func calc(){
-        var num = Double()
-        print(numTF.text!)
-        if let tempnum = Double.init(numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")){
-            num = tempnum
+    func deleteNum(){
+        if let numText = numTF.text{
+            if numText.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "1234567890.")) != ""{
+                numTF.text = ""
+            }else{
+                numTF.text = String(numText.characters.dropLast(1))
+            }
         }
-        tempArray.addObject(Double(num))
+    }
+    
+    func calc(){
+        var num = String()
+        print(numTF.text!)
+        if let _ = Double.init(numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")){
+            num = numTF.text!.stringByReplacingOccurrencesOfString(",", withString: "")
+        }
+        tempArray.addObject(num)
         numTF.text = nil
         if tempArray.count == symbolArray.count{
             symbolArray.removeLastObject()
@@ -309,9 +439,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         break
                     case 3:
                         let index = symbolArray.indexOfObject(symbol)
-                        let first = tempArray[index].doubleValue
-                        let last = tempArray[index+1].doubleValue
-                        tempArray[index] = first + last
+                        let first = tempArray[index] as! String
+                        let last = tempArray[index+1] as! String
+                        tempArray[index] = first ++ last
                         tempArray.removeObjectAtIndex(index+1)
                         symbolArray.removeObjectAtIndex(index)
                         break
